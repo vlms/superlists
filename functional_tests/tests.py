182b92c5e145342f5bfd6cc151d3988f3479ed8f
Vilmis Goat <v.test.goat@gmail.com>
2016-01-28 11:45:29 +0200

    Base template uses static tag

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -102,3 +102,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
             512,
             delta=5
         )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+        )
