5978791a7c6ba47fe2c5a886c5ec8b4b14192a7e
Vilmis Goat <v.test.goat@gmail.com>
2016-03-03 10:25:27 +0200

    Custom Persona auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -83,6 +83,10 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
