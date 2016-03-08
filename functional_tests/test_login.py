0fdc69eb21805d937cfe3724cabeb19618c4ecba
Vilmis Goat <v.test.goat@gmail.com>
2016-03-08 12:16:22 +0200

    placeholder test_my_lists and move login checkers into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,7 +1,7 @@
 import time
-from selenium.webdriver.support.ui import WebDriverWait
 
 from .base import FunctionalTest
+TEST_EMAIL = 'edith@mockmyid.com'
 
 
 class LoginTest(FunctionalTest):
@@ -19,16 +19,27 @@ class LoginTest(FunctionalTest):
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid', navbar.text)
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
+
+        # Refreshing the page, she sees it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
+
+        # Terrifiedof this new feature, she reflexively clicks "logout"
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
+
+        # The "logged out" status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
@@ -40,8 +51,3 @@ class LoginTest(FunctionalTest):
             retries -= 1
             time.sleep(0.5)
         self.fail('could not find window')
-
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
-        )
